a6e298abc023ec3fb0d5eec156809697e82cd6ef
Harriss1989 <harriss_1@hotmail.co.uk>
2022-02-28 19:05:10 +0000

    Add run game function

---
+++ questions.py
@@ -0,0 +1,140 @@
+quiz_questions = [
+    {
+        "question": "What make is the car in which Marty Mcfly \
+             uses too travel back in time?:",
+        "answers": ["B.M.W", "DMC Delorian", "Ford"],
+        "correct": "DMC Delorian"
+    },
+    {
+        "question": "According to Doctor Emmit Brown what makes \
+             time travel possible?:",
+        "answers": ["The rotation of the earth ",
+                    "Newtons law of motion",
+                    "The flux capacitor"],
+        "correct": "The flux capacitor"
+    },
+    {
+        "question": "How much power does the time machine need \
+             to send Marty back in time?:",
+        "answers": ["1.21 gigawatts", "2000 megawatts", "55 volts"],
+        "correct": "1.21 gigawatts"
+    },
+    {
+        "question": "What date does Marty Mcfly travel back too?:",
+        "answers": ["September 26th 1989",
+                    "November 5th 1955",
+                    "December 25th 1965"],
+        "correct": "November 5th 1955"
+    },
+    {
+        "question": "What is the name of the dance Marty attends where \
+            his mom and dad kiss for the first time?:",
+        "answers": ["The fish under the sea dance",
+                    "The monkey in the jungle dance",
+                    "The enchantment under the sea dance"],
+        "correct": "The enchantment under the sea dance"
+    },
+    {
+        "question": "what is the name of the mail marty meets \
+             doctor emmit brown called?:",
+        "answers": ["twin pine mall",
+                    "lonley pine mall",
+                    "westfilds"],
+        "correct": "twin pine mall"
+    },
+    {
+        "question": "at what time does the lightning strike the clock tower",
+        "answers": ["12:00pm",
+                    "10:50am",
+                    "10:04pm"],
+        "correct": "10:04pm"
+    },
+    {
+        "question": "what is the name of the mayor in martys time?:",
+        "answers": ["Mayor Goldburge Jenson",
+                    "Mayor Goldie Wilson",
+                    "mayor Biff Tannan"],
+        "correct": "Mayor Goldie Wilson"
+    },
+    {
+        "question": "What is the name of the main antaganist?:",
+        "answers": ["Geff Bazos",
+                    "Biff Tannan",
+                    "George Mcfly"],
+        "correct": "Biff Tannan"
+    },
+    {
+        "question": "what is the name of the town matry lives at?:",
+        "answers": ["Hill vally",
+                    "Springfield",
+                    "Riverdale"],
+        "correct": "Hill vally"
+    },
+    {
+        "question": "What Song Did Marty And His Band \
+            Play For The Dance Auditions?:",
+        "answers": ["Power of love",
+                    "Taninted love",
+                    "I will do anything for love"],
+        "correct": "Power of love"
+    },
+    {
+        "question": "What is the name of Doc Browns dog in 1985?:",
+        "answers": ["Einstein",
+                    "Tesla",
+                    "Newton"],
+        "correct": "Einstein"
+    },
+    {
+        "question": "What Was The Name Of Marty's Band?:",
+        "answers": ["The Pinheads",
+                    "The Hill Vally Rockers",
+                    "The Who"],
+        "correct": "The Pinheads"
+    },
+    {
+        "question": "What Color Was Marty's Calvin Klein Underwear?:", 
+        "answers": ["Red",
+                    "White",
+                    "Purple"],
+        "correct": "Purple"
+    },
+    {
+        "question": "From whom did Doc obtain the plutonium \
+             he needed to use the time machine?:",
+        "answers": ["Lybians",
+                    "English",
+                    "Russians"],
+        "correct": "Lybian"
+    },
+    {
+        "question": "What speed did the Delorian \
+             need to reach to send someone back in time?:",
+        "answers": ["77MPH", "78MPH", "88MPH"],
+        "correct": "88MPH"
+    },
+    {
+        "question": "What did Doc Brown hit his head off \
+            when he invented the flux capacitor??:",
+        "answers": ["The sink in his toilet",
+                    "The cupbord door in his kitchen",
+                    "The bonnet on his car"],
+        "correct": "The sink in his toilet"
+    },
+    {
+        "question": "What phrase does the Doc repeat throughout \
+             the trilogy??:",
+        "answers": ["Great Scott!",
+                    "Na-Nu Na-Nu Shazbot",
+                    "Danger, Will Robinson"],
+        "correct": "Great Scott!"
+    },
+    {
+        "question": "What is the name of Marty's girlfriend \
+            in Back to the Future?:",
+        "answers": ["Jennifer",
+                    "Alison",
+                    "Betty"],
+        "correct": "Jennifer"
+    }
+]
