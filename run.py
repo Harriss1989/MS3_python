5e246f7eae5c78f150dbb882ab7a1fe965228120
Harriss1989 <harriss_1@hotmail.co.uk>
2022-02-28 16:19:08 +0000

    Add questions to quiz

--- run.py
+++ run.py
@@ -1,3 +1,42 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+quiz = [
+    {
+        "question" : "What make is the car in which Marty Mcfly uses too travel back in time?:",
+        "answers" : ["B.M.W", "DMC Delorian", "Ford"],
+        "correct" : "DMC Delorian"
+    },
+    {
+        "question" : "accroding to Doctor Emmit Brown what makes time travel possible?:",
+        "answers" : ["the rotation of the earth ", "nutons law of motion", "the flux capacitor"],
+        "correct" : "the flux capacitor"
+    },
+    {
+        "question" : "how much power does the time machine need to send marty back in time?:",
+        "answers" : ["1.21 gigawatts", "2000 megawatts", "55 volts"],
+        "correct" : "1.21 gigawatts"
+    },
+    {
+        "question" : "what date does marty mcfly travel back too?:",
+        "answers" : ["september 26th 1989", "november 5th 1955", "december 25th 1965"],
+        "correct" : "november 5th 1955"
+    },
+    {
+        "question" : "what is the name of the dance marty attends where his mom and dad kiss for the first time?:",
+        "answers" : ["the fish under the sea dance", "the monkey in the jungle dance", "the enchantment under the sea dance"],
+        "correct" : "the enchantment under the sea dance"
+    },
+    {
+    }
+
+]
+    
+print("▒▒h       
+                                          ,╓╓╓╓╓╓╓╓╓╓╓╓▒▒▒▒╓m$ÖÖQⁿ═w, ,▒▀Ç ,▄█████
+                                     ,▄╗╢▒▄███████████▌▒▒▒▌▒█▒▒@▒▒▒▒╢╢▒████████░░█▌
+                                 ▄mÑ▒▄████████████████▌▒▒▒█▒████▓██▓▓▓╬▓▒▒▒▀▀▀████▌
+                     ,,,╓╓╖╥╖╥R▒▒▒▒███▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▀▒▒▒▒▒░█
+       ,╓╗@@@╣╢╢▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒[
+      ╓░░▒▒▒@▒▒▒░█████████░▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀░█████████░▀▒▒▒▒▒▒▒███
+     ┌╢▒▒╣█▒@█▒░███▓▓▓▓▓███▌║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒╢╢╢╢╢╢╜░██▀▓▓▓▓▓███▌║▒▒▒▄████▀
+        ████▄▄▄██▌╢╣»è⌐╢╣███▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄██▌╣╣»è⌐╢╣███▄███████r
+       ▀▀▀▀▀▀▀▀▀▀█▓╣▓▓▓╣▓█████████████████████████████████████▓╣▓▓▓╣▓███▀▀` ▀'
+                 ▀▀███████▀                                 ▀███████▀▀")
