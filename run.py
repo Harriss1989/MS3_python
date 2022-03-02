1848a3e592cce91c7e653ee375b77c29c706938a
Harriss1989 <harriss_1@hotmail.co.uk>
2022-03-02 17:33:39 +0000

    updated run game function to take in player name and print back to the terminal

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-import gspread
 import random
+import gspread
 from google.oauth2.service_account import Credentials
 from questions import quiz_questions
 
@@ -24,31 +24,63 @@ data = scores.get_all_values()
 
 def run_game():
     """
-    Game start with welcome message and logo
+    Game welcome message, store user name and return input
     """
-    print("Welcome to the Back To The Future Quiz")
-    game_option()
-
-
-def game_option():
-    """
-    user input to play game, see rules,see ScoreBored
-    """
-    print("Pick from the following options")
     print("""
-    <         PlayQuiz        >
-    <           Rules         >
-    <        ScoreBoard       >\n
-    Enter 'P' to start game, 'R' to read rules, 'S' for ScoreBoard.""")
-    player_name_input()
+    Welcome to the Back To The Future Quiz. This quiz is for fans
+    of the Back To The Future films.
+    Test your knowledge to see if you are a true fan with this fun quiz.
+    You can pick from 10, 15 or 20 questions.
+    Questions will be mutiple choice,
+    there will be 3 answers to choose from A, B or C
+    let's start
+    """)
+    name = input("Enter your name time traveller: ")
+    name = name.capitalize()
+    name = name.strip()
+    while len(name) == 0:
+        print(" ")
+        print("Great Scott! Time traveller, we didn't catch your name")
+        print(" ")
+        name = input("Enter your name time traveller: ")
+        name = name.capitalize()
+        name = name.strip()
+    else:
+        print("Time traveller " + name + " Good luck, lets begin!")
+    start_game()
 
 
-def player_name_input():
-    """
-    gets player name for game
-    """
-    name = input('Enter your name: ')
-    print('Welcome ' + name)
+# def game_option():
+#     """
+#     user input to play game, see rules,see ScoreBored
+#     """
+#     print("Pick from the following options")
+#     print("""
+#     <         PlayQuiz        >
+#     <           Rules         >
+#     <        ScoreBoard       >\n
+#     Enter 'P' to play game, 'R' to read rules, 'S' for ScoreBoard.""")
+    # player_name_input()
+    # start_game()
+
+
+# def start_game():
+#     """
+#     Adds random question 
+#     """
+#     questions_length = len(quiz_questions)
+#     random_question = random.shuffle(questions_length) 
+#     return random_question
+#     print(random_question)
+
+
+
+# def player_name_input():
+#     """
+#     gets player name for game
+#     """
+#     name = input('Enter your name: ')
+#     print('Welcome ' + name)
 
 
 run_game()
